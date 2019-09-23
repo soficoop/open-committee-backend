d99f57f9a05b870caa839c63d4a0b333257b3f5d
Aviran Katz <aviran@soficoop.com>
2019-09-23 18:06:21 +0300

    HS-9 strapi support

--- api/meeting/controllers/Meeting.js
+++ api/meeting/controllers/Meeting.js
@@ -1,3 +1,34 @@
 'use strict';
+const parseTemplate = require('../../../config/functions/template');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Emails meeting to its subscribers
+   * @param {import("koa").Context} ctx Koa context
+   * @param {any?} meeting Meeting to send email about (optional)
+   */
+  async emailSubscribers(ctx) {
+    const meeting = await strapi.services.meeting.findOne({ id: ctx.params.id });
+    const { subscribedUsers } = await strapi.services.committee.findOne({ id: meeting.committee.id });
+    for (const user of subscribedUsers) {
+      await strapi.plugins.email.services.email.send({
+        to: user.email,
+        subject: 'ישיבה חדשה במערכת ועדה פתוחה',
+        html: await parseTemplate('public/templates/NewMeeting.html', { meeting, user })
+      });
+    }
+    ctx.res.statusCode = 200;
+    return ctx.status;
+  },
+
+  /**
+   * Creates a meeting
+   * @param {import("koa").ParameterizedContext} ctx Koa context
+   */
+  async create(ctx) {
+    const meeting = await strapi.services.meeting.create(ctx.request.body);
+    ctx.params.id = meeting.id;
+    await strapi.controllers.meeting.emailSubscribers(ctx);
+    return meeting;
+  }
+};
---
+++ config/functions/template.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+const handlebars = require('handlebars');
+
+handlebars.registerHelper('formatDate', (date) => {
+  const dateList = date.toISOString().split('T')[0].split('-');
+  return `${dateList[2]}.${dateList[1]}.${dateList[0]}`;
+});
+
+handlebars.registerHelper('appUrl', () => strapi.config.environments.development.appUrl);
+
+module.exports = async (templateFile, doc) => {
+  const data = await fs.readFile(templateFile, 'utf8');
+  const template = handlebars.compile(data);
+  return template(doc);
+};

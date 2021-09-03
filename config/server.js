36703335075713c531a66a5cbed1fed6791bfe32
Aviran Katz <aviran.katz@gmail.com>
2021-09-03 15:33:43 +0300

    feat: allow url prefix for strapi

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL_PREFIX', ''),
   templatesDir: 'public/templates/',
   appUrl: env('APP_URL', 'https://app.sviva.net'),
   strapiUrl: env('STRAPI_URL', 'https://admin.oc.soficoop.com'),
